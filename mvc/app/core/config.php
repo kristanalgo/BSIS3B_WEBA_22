7386cc37e938f848b5bb8ded7eb73e544742d327
abel-palero <abelochavillo.palero@gmail.com>
2024-04-12 09:57:45 +0800

    mvc framework

---
+++ mvc/app/core/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('APP_NAME', 'PET CLINIC');
+define('ROOT', 'http://localhost/bsis3b_weba_24/mvc/public');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvc_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
